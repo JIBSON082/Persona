19c9f9bde57da28afc401a7ddf50405109ff24b7
JIBSON082 <105949776+JIBSON082@users.noreply.github.com>
2026-06-06 11:23:39 +0100

    Update stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,5 +1,4 @@
 import Stripe from "stripe";
-
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2023-10-16",
   typescript: true,
@@ -58,4 +57,3 @@ export async function createBillingPortalSession(
 
   return session.url;
 }
-
